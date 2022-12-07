c47557b8a80b483edfadd29b0f588f7d50d53a8b
David Lounsbrough <david.lounsbrough@gmail.com>
2022-12-07 14:17:19 -0600

    Adding lint and refactoring a bit

--- 2022/8/index.js
+++ 2022/8/index.js
@@ -1,54 +1,55 @@
-import input from "./input.js";
+import input from './input';
 
-const inputLines = input.split("\n").filter((o) => o);
+const inputLines = input.split('\n').filter((o) => o);
 
-let currentDirectory = "/";
+let currentDirectory = '/';
 const directorySizes = {};
 
 const changeDirectory = (path) => {
-  if (path === "/") {
-    currentDirectory = "/";
-  } else if (path === "..") {
-    currentDirectory =
-      currentDirectory.split("/").slice(0, -1).join("/") || "/";
+  if (path === '/') {
+    currentDirectory = '/';
+  } else if (path === '..') {
+    currentDirectory = currentDirectory.split('/').slice(0, -1).join('/') || '/';
   } else {
-    currentDirectory = [...currentDirectory.split("/"), path]
-      .join("/")
-      .replaceAll("//", "/");
+    currentDirectory = [...currentDirectory.split('/'), path]
+      .join('/')
+      .replaceAll('//', '/');
   }
 };
 
+const addFileSizeToDirectory = (directory, fileSize) => {
+  directorySizes[directory] = (directorySizes[directory] || 0) + fileSize;
+};
+
 const addFileSizeToDirectoryTree = (fileSize) => {
-  if (currentDirectory === "/") {
-    directorySizes[currentDirectory] =
-      (directorySizes[currentDirectory] || 0) + parseInt(fileSize, 10);
+  if (currentDirectory === '/') {
+    addFileSizeToDirectory(currentDirectory, fileSize);
     return;
   }
 
-  const treeDirectories = currentDirectory.split("/");
+  const treeDirectories = currentDirectory.split('/');
 
-  for (let i = 0; i < treeDirectories.length; i++) {
-    const directory = treeDirectories.slice(0, i + 1).join("/") || "/";
-    directorySizes[directory] =
-      (directorySizes[directory] || 0) + parseInt(fileSize, 10);
+  for (let i = 0; i < treeDirectories.length; i += 1) {
+    const directory = treeDirectories.slice(0, i + 1).join('/') || '/';
+    addFileSizeToDirectory(directory, fileSize);
   }
 };
 
 inputLines.forEach((outputLine) => {
-  const isCommand = outputLine.startsWith("$");
+  const isCommand = outputLine.startsWith('$');
 
   if (isCommand) {
-    const isDirectoryChange = outputLine.startsWith("$ cd");
+    const isDirectoryChange = outputLine.startsWith('$ cd');
 
     if (isDirectoryChange) {
-      const path = outputLine.replace(/\$ cd /i, "");
+      const path = outputLine.replace(/\$ cd /i, '');
       changeDirectory(path);
     }
   } else {
-    const isFile = !outputLine.startsWith("dir");
+    const isFile = !outputLine.startsWith('dir');
 
     if (isFile) {
-      const [fileSize] = outputLine.split(" ");
+      const fileSize = parseInt(outputLine.split(' ')[0], 10);
       addFileSizeToDirectoryTree(fileSize);
     }
   }
@@ -56,21 +57,24 @@ inputLines.forEach((outputLine) => {
 
 const diskSize = 70_000_000;
 const requiredSpace = 30_000_000;
-const usedSpace = directorySizes["/"];
+const usedSpace = directorySizes['/'];
 const freeSpace = diskSize - usedSpace;
 const missingSpace = requiredSpace - freeSpace;
+const fileSizes = Object.values(directorySizes);
 
-const sumOfSmallDirectories = Object.entries(directorySizes).reduce(
-  (total, [, fileSize]) => (fileSize <= 100000 ? total + fileSize : total),
-  0
+const sumOfSmallDirectories = fileSizes.reduce(
+  (total, fileSize) => (fileSize <= 100000 ? total + fileSize : total),
+  0,
 );
 
-const smallestDirectoryWithRequiredSpace = Object.entries(directorySizes)
-  .filter(([, fileSize]) => fileSize >= missingSpace)
-  .sort((a, b) => a[1] - b[1])[0][1];
+const smallestDirectoryWithRequiredSpace = fileSizes
+  .filter((fileSize) => fileSize >= missingSpace)
+  .sort((a, b) => a - b)[0];
 
 console.log({
-  directorySizes,
-  sumOfSmallDirectories,
-  smallestDirectoryWithRequiredSpace
+  extraData: { directorySizes },
+  solution: {
+    part1: { sumOfSmallDirectories },
+    part2: { smallestDirectoryWithRequiredSpace },
+  },
 });
