68e657ad3b3662ccc5b365623c355d3754473d97
David Lounsbrough <david.lounsbrough@gmail.com>
2022-12-10 13:56:54 -0600

    2021 day 2

---
+++ 2021/2/index.js
@@ -0,0 +1,38 @@
+import input from './input';
+
+let currentHorizontalPosition = 0;
+let currentDepth1 = 0;
+let currentDepth2 = 0;
+let currentAim = 0;
+
+const commands = input.split('\n').filter(Boolean)
+  .map((inputLine) => {
+    const regexMatch = inputLine.match(/([^\s]*) (\d*)/);
+    return ({ direction: regexMatch[1], distance: parseInt(regexMatch[2], 10) });
+  });
+
+commands.forEach(({ direction, distance }) => {
+  switch (direction) {
+    case 'forward':
+      currentHorizontalPosition += distance;
+      currentDepth2 += distance * currentAim;
+      break;
+    case 'up':
+      currentDepth1 -= distance;
+      currentAim -= distance;
+      break;
+    case 'down':
+      currentDepth1 += distance;
+      currentAim += distance;
+      break;
+    default:
+      throw new Error('Unknown direction');
+  }
+});
+
+console.log({
+  solution: {
+    part1: currentHorizontalPosition * currentDepth1,
+    part2: currentHorizontalPosition * currentDepth2,
+  },
+});
