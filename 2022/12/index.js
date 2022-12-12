3a1c6cfa9624ee05b196908bcdaf1bcdc111de40
David Lounsbrough <david.lounsbrough@gmail.com>
2022-12-12 14:39:51 -0600

    working, needs a little refactor

--- 2022/12/index.js
+++ 2022/12/index.js
@@ -1,9 +1,11 @@
-import input from './demo';
+import input from './input';
 
 const alphabetGrid = input.split('\n').filter(Boolean).map((inputLine) => inputLine.split(''));
 
-const startHeight = 0;
-const endHeight = 27;
+const startHeight = 1;
+const endHeight = 26;
+
+const isInRange = (x, [min, max]) => x >= min && x <= max;
 
 const letterToHeight = (letter) => {
   if (letter === 'S') {
@@ -19,50 +21,116 @@ const letterToHeight = (letter) => {
 
 const heightGrid = alphabetGrid.map((alphabetGridLine) => alphabetGridLine.map(letterToHeight));
 
-console.log(heightGrid);
-
-const startLineIndex = heightGrid.findIndex((heightGridLine) => heightGridLine
-  .some((height) => height === startHeight));
-const endLineIndex = heightGrid.findIndex((heightGridLine) => heightGridLine
-  .some((height) => height === endHeight));
+const startLineIndex = alphabetGrid.findIndex((alphabetGridLine) => alphabetGridLine
+  .some((letter) => letter === 'S'));
+const endLineIndex = alphabetGrid.findIndex((alphabetGridLine) => alphabetGridLine
+  .some((letter) => letter === 'E'));
 
 const startPosition = [
-  heightGrid[startLineIndex].findIndex((height) => height === startHeight),
+  alphabetGrid[startLineIndex].findIndex((letter) => letter === 'S'),
   startLineIndex,
 ];
 
 const endPosition = [
-  heightGrid[endLineIndex].findIndex((height) => height === endHeight),
+  alphabetGrid[endLineIndex].findIndex((letter) => letter === 'E'),
   endLineIndex,
 ];
 
-let pathBranches = [{
-  steps: [startPosition], reachedEnd: false, looped: false, deadEnd: false,
-}];
+const aPositions = [];
+alphabetGrid.forEach((alphabetGridLine, y) => {
+  alphabetGridLine.forEach((letter, x) => {
+    if (letter === 'a') {
+      aPositions.push([x, y]);
+    }
+  });
+});
+
+let pathBranches;
 let currentPathBranches;
+let newPathsFound;
+let pathsThatReachedEnd;
+let visitedPositions;
+
+const getPossibleNextSteps = (branchIndex) => {
+  const currentBranch = currentPathBranches[branchIndex];
+
+  const [lastX, lastY] = currentBranch.steps.at(-1);
+
+  return [
+    [lastX, lastY - 1], [lastX, lastY + 1], [lastX - 1, lastY], [lastX + 1, lastY],
+  ].filter(([nextX, nextY]) => !visitedPositions
+    .some(([prevX, prevY]) => prevX === nextX && prevY === nextY)
+    && isInRange(nextX, [0, heightGrid[0].length - 1])
+    && isInRange(nextY, [0, heightGrid.length - 1])
+    && heightGrid[nextY][nextX] <= heightGrid[lastY][lastX] + 1);
+};
 
 const getNextBranches = (branchIndex) => {
-  const possibleNextSteps = [[1, 0], [0, 1]];
+  const currentBranch = currentPathBranches[branchIndex];
 
-  // dead end? !possibleNextSteps.length
+  const possibleNextSteps = getPossibleNextSteps(branchIndex);
+
+  if (!possibleNextSteps.length) {
+    return;
+  }
+
+  newPathsFound = true;
 
   const newBranches = [];
   possibleNextSteps.forEach((possibleNextStep) => {
-    // reached end? possibleNextStep is endPosition
-    // looped? possibleNextStep already exists in array
-    newBranches.push({ steps: [...currentPathBranches[branchIndex].steps, possibleNextStep] });
+    visitedPositions.push(possibleNextStep);
+    const reachedEnd = possibleNextStep[0] === endPosition[0]
+      && possibleNextStep[1] === endPosition[1];
+    if (reachedEnd) {
+      pathsThatReachedEnd.push({ steps: [...currentBranch.steps, possibleNextStep] });
+    } else {
+      newBranches.push({ steps: [...currentBranch.steps, possibleNextStep] });
+    }
   });
 
   pathBranches.push(...newBranches);
 };
 
-console.log(JSON.stringify(pathBranches));
-currentPathBranches = [...pathBranches];
-pathBranches = [];
-getNextBranches(0);
-console.log(JSON.stringify(pathBranches));
-currentPathBranches = [...pathBranches];
-pathBranches = [];
-getNextBranches(0);
-getNextBranches(1);
-console.log(JSON.stringify(pathBranches));
+pathBranches = [{ steps: [startPosition] }];
+newPathsFound = true;
+pathsThatReachedEnd = [];
+visitedPositions = [];
+while (newPathsFound) {
+  currentPathBranches = [...pathBranches];
+  pathBranches = [];
+  newPathsFound = false;
+  currentPathBranches.forEach((_, branchIndex) => getNextBranches(branchIndex));
+}
+
+const minimumStepsToEndFromS = pathsThatReachedEnd
+  .sort((a, b) => a.steps.length - b.steps.length)[0].steps.length - 1;
+
+let minimumStepsToEndFromAnyA = Infinity;
+aPositions.forEach((aPosition) => {
+  pathBranches = [{ steps: [aPosition] }];
+  newPathsFound = true;
+  pathsThatReachedEnd = [];
+  visitedPositions = [];
+  while (newPathsFound) {
+    currentPathBranches = [...pathBranches];
+    pathBranches = [];
+    newPathsFound = false;
+    currentPathBranches.forEach((_, branchIndex) => getNextBranches(branchIndex));
+  }
+  const shortestPathToEndFromA = pathsThatReachedEnd
+    .sort((a, b) => a.steps.length - b.steps.length)[0];
+
+  if (shortestPathToEndFromA) {
+    minimumStepsToEndFromAnyA = Math.min(
+      minimumStepsToEndFromAnyA,
+      shortestPathToEndFromA.steps.length - 1,
+    );
+  }
+});
+
+console.log({
+  solution: {
+    part1: { minimumStepsToEndFromS },
+    part2: { minimumStepsToEndFromAnyA },
+  },
+});
