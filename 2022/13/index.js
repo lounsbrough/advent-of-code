956216d82ace58ef433e73943553ca0115116422
David Lounsbrough <david.lounsbrough@gmail.com>
2022-12-13 16:37:04 -0600

    part 2, yay

--- 2022/13/index.js
+++ 2022/13/index.js
@@ -56,9 +56,36 @@ const isPacketPairOutOfOrder = ([firstPacket, secondPacket]) => {
 const packetsPairsOutOfOrder = packetPairs
   .map((packetPair) => isPacketPairOutOfOrder(packetPair) || false);
 
+const dividerPackets = [[[2]], [[6]]];
+const allPacketsPlusDividers = [...packetPairs.flat(), ...dividerPackets];
+
+let packetsNeedShifting = true;
+while (packetsNeedShifting) {
+  packetsNeedShifting = false;
+
+  for (let i = 0; i < allPacketsPlusDividers.length - 1; i += 1) {
+    const [firstPacket, secondPacket] = allPacketsPlusDividers.slice(i, i + 2);
+
+    const isOutOfOrder = isPacketPairOutOfOrder([firstPacket, secondPacket]);
+
+    if (isOutOfOrder) {
+      allPacketsPlusDividers[i] = secondPacket;
+      allPacketsPlusDividers[i + 1] = firstPacket;
+      packetsNeedShifting = true;
+    }
+  }
+}
+
+const sumOfCorrectPackets = packetsPairsOutOfOrder
+  .reduce((sum, outOfOrder, i) => (outOfOrder ? sum : sum + i + 1), 0);
+
+const decoderKey = allPacketsPlusDividers
+  .reduce((product, packet, index) => (dividerPackets
+    .map(JSON.stringify).includes(JSON.stringify(packet)) ? product * (index + 1) : product), 1);
+
 console.log({
   solution: {
-    part1: packetsPairsOutOfOrder
-      .reduce((sum, outOfOrder, i) => (outOfOrder ? sum : sum + i + 1), 0),
+    part1: { sumOfCorrectPackets },
+    part2: { decoderKey },
   },
 });
