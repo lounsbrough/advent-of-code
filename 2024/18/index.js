b4b2e874c2efca163abbb04180989ddb5df8fcb9
David Lounsbrough <david.lounsbrough@gmail.com>
2025-01-07 11:53:22 -0600

    part 2 is too slow

--- 2024/18/index.js
+++ 2024/18/index.js
@@ -1,19 +1,16 @@
 import input from './input.js';
 
+const gridSize = 71;
+const initialByteCount = 1024;
+
 const bytes = input.split('\n').filter(Boolean).map((line) => line.split(',').map(Number));
 
-const gridSize = 71;
 const grid = Array.from({ length: gridSize }, () => Array.from({ length: gridSize }, () => '.'));
-
-console.log(bytes);
-
-bytes.slice(0, 1024).forEach((byte) => {
+bytes.slice(0, initialByteCount).forEach((byte) => {
   grid[byte[1]][byte[0]] = '#';
 });
 
-console.log(grid.map((g) => g.join('')));
-
-const minStepsToPosition = {};
+let minStepsToPosition = {};
 const findMinStepsToEnd = (position, steps) => {
   const positionKey = position.join(',');
   if (steps >= minStepsToPosition[positionKey]) {
@@ -40,4 +37,16 @@ const findMinStepsToEnd = (position, steps) => {
 
 const part1 = findMinStepsToEnd([0, 0], 0);
 
-console.log({ solution: { part1, part2: '?' } });
+let closingByte = initialByteCount - 1;
+let closed = false;
+while (!closed) {
+  closingByte += 1;
+  const nextByte = bytes[closingByte];
+  grid[nextByte[1]][nextByte[0]] = '#';
+  minStepsToPosition = {};
+  closed = (findMinStepsToEnd([0, 0], 0) === Infinity);
+}
+
+const part2 = bytes[closingByte];
+
+console.log({ solution: { part1, part2 } });
